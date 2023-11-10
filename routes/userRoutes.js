7713deb08209a613989adf8c26f76e101a48583f
Arfaoui-ghaith <53495319+Arfaoui-ghaith@users.noreply.github.com>
2023-11-10 15:52:57 +0100

    Update userRoutes.js
    push

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('./../controllers/authContoller');
 const userController = require('./../controllers/userController');
 
-router.route('/signup').post(authController.protect,authController.signup);
+router.route('/signup').post(authController.signup);
 router.route('/signin').post(authController.login);
 
 router.route('/').get(authController.protect,userController.getUsers);
